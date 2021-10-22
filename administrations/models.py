a33e75d4793b312db4de253e940b19a836afcd00
AleksTankist111 <aleks.skakun1996@yandex.ru>
2021-10-22 20:45:00 +0200

    Implemented tumblr for swapping registration form (user\admin)

--- administrations/models.py
+++ administrations/models.py
@@ -1,16 +1,14 @@
 from django.db import models
-
+from initiatives.models import Initiative
+import datetime
 # Create your models here.
 
 
-# class AdministrationProfile(models.Model):
-#     first_name = models.CharField('First Name', max_length=100, blank=False)
-#     last_name = models.CharField('Last Name', max_length=100, blank=False)
-#     role = models.CharField('Role', max_length=100)
-#     password = models.CharField('Password', max_length=100)
-#     email = models.CharField('Email', max_length=100, unique=True)
-#     organization = models.CharField('Organization', max_length=100)
-#     address = models.CharField('Address', max_length=100)
-#     city = models.CharField('city', max_length=100)
+class Statistics:
+
+    def number_of_new_initiatives(self, timedur):
+        return Initiative.objects.filter(PublicationDate__level__gte=datetime.datetime.now()-timedur).count()
+
+    # TODO: seaborn implement to show on the screen. Or mb
 
 # class Achievement
